86d4124fd42cdad401aba60c2e2322b4725a2a46
Terry Moore <terry.moore.ii@gmail.com>
2013-09-04 20:23:40 -0400

    GUI lookin good

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ var cheerio = require('cheerio');
 var clc = require('cli-color');
 var xcolor = require('xcolor');
 
+var keypress = require('keypress');
+var title = 'Hacker News';
+
+var terminalWidth = process.stdout.getWindowSize()[0]
+// make `process.stdin` begin emitting "keypress" events
+//keypress(process.stdin);
+
+// listen for the "keypress" event
+// process.stdin.on('keypress', function (ch, key) {
+//   console.log('got "keypress"', key);
+//   if (key && key.ctrl && key.name == 'c') {
+//     process.stdin.pause();
+//   }
+// });
+
+//process.stdin.setRawMode(true);
+//process.stdin.resume();
+  
 var options = {
   host: 'news.ycombinator.com',
   path: '/news',
@@ -20,24 +38,23 @@ callback = function(response) {
 
   response.on('end', function () {
   	parsePage(str);
-  	//console.log
-
   });
 }
 
 http.request(options, callback).end();
 
-
 var parsePage = function(data){
 	var $ = cheerio.load(data);
 	var items = [];
+    header(title)
     $('td.title > a').each(function(k,v){
     	var $this = $(this);
     	if ($this.text().toLowerCase() === 'more') return;
     	var item = {
-    		title: $this.text(),
-    		href: $this.attr('href'),
-    		points: $this.parent('td').parent('tr').next('tr').children('td:nth-child(2)').children('span').html()
+            id: k<10 ? (" " + k + ". ") : (k + ". "),
+    		title: $this.text() || '',
+    		href: $this.attr('href') || '',
+    		points: $this.parent('td').parent('tr').next('tr').children('td:nth-child(2)').children('span').html() || "0 points"
     	};
     	//items.push(item);
 
@@ -45,7 +62,24 @@ var parsePage = function(data){
     });
 };
 
-var display = function(item){
-
-	xcolor.log('{{bg #FFFFFF}}{{#000000}}' + item.title)
+var rowWidth = function(item){
+  return terminalWidth 
+    - (item.id.length)
+    - (item.title.length) 
+    - (item.points.length + 2 );
 }
+
+var rowSpace = function(size){
+  var s = "";
+  for (var i=0; i<size; i++)
+    s += " "
+  return s;
+}
+
+var header = function(title){
+  xcolor.log('{{bg #ff6600}}{{#000000}}' + title + rowSpace(terminalWidth - title.length ));
+}
+var display = function(item){
+	xcolor.log('{{bg #FFFFFF}}{{#000000}}' + item.id + item.title + rowSpace(rowWidth(item)) + '(' + item.points + ')' );
+}
+
